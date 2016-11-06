0f81d1f0c0eb49db68f29d774ac986b30b86608b
Nilotaviano <nilotaviano@gmail.com>
2016-11-06 16:34:03 -0300

    Done

---
+++ Idleminer/Idleminer/LastInputInfo.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using System.Runtime.InteropServices;
+
+namespace Win32_API
+{
+    /// <summary>
+    /// http://www.codeproject.com/Articles/13384/Getting-the-user-idle-time-with-C
+    /// </summary>
+    public class Win32
+    {
+        private struct LASTINPUTINFO
+        {
+            public uint cbSize;
+
+            public uint dwTime;
+        }
+
+        [DllImport("User32.dll")]
+        public static extern bool LockWorkStation();
+
+        [DllImport("User32.dll")]
+        private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+        [DllImport("Kernel32.dll")]
+        private static extern uint GetLastError();
+
+        public static uint GetIdleTime()
+        {
+            LASTINPUTINFO lastInPut = new LASTINPUTINFO();
+            lastInPut.cbSize = (uint)System.Runtime.InteropServices.Marshal.SizeOf(lastInPut);
+            GetLastInputInfo(ref lastInPut);
+
+            return ((uint)Environment.TickCount - lastInPut.dwTime);
+        }
+
+        public static long GetTickCount()
+        {
+            return Environment.TickCount;
+        }
+
+        public static long GetLastInputTime()
+        {
+            LASTINPUTINFO lastInPut = new LASTINPUTINFO();
+            lastInPut.cbSize = (uint)System.Runtime.InteropServices.Marshal.SizeOf(lastInPut);
+            if (!GetLastInputInfo(ref lastInPut))
+            {
+                throw new Exception(GetLastError().ToString());
+            }
+
+            return lastInPut.dwTime;
+        }
+
+
+    }
+
+}
---
+++ Idleminer/Idleminer/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Win32_API;
+
+namespace Idleminer
+{
+    class Program
+    {
+        private const string NO_PATH_SPECIFIED_MESSAGE = "No path specified";
+
+        static void Main(string[] args)
+        {
+            if(args.Count() < 1)
+            {
+                Console.WriteLine(NO_PATH_SPECIFIED_MESSAGE);
+                Console.ReadKey();
+
+                return;
+            }
+
+            var timeToTrigger = new TimeSpan(hours: 0, minutes: 5, seconds: 0);
+            var sleepTime = new TimeSpan(hours: 0, minutes: 1, seconds: 0);
+
+            var processInfo = new ProcessStartInfo(args.First());
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+
+            Process process = null; 
+
+            do
+            {
+                var idleTime = Win32.GetIdleTime();
+
+                if (idleTime < timeToTrigger.TotalMilliseconds)
+                {
+                    process?.Close();
+                    process = null;
+
+                    Thread.Sleep((int)sleepTime.TotalMilliseconds);
+                }
+                else
+                {
+                    if (process == null)
+                    {
+                        process = Process.Start(processInfo);
+
+                        process.OutputDataReceived += (object sender, DataReceivedEventArgs e) =>
+                            Console.WriteLine("output>>" + e.Data);
+                        process.BeginOutputReadLine();
+
+                        process.ErrorDataReceived += (object sender, DataReceivedEventArgs e) =>
+                            Console.WriteLine("error>>" + e.Data);
+                    }
+                    else
+                    {
+                        Thread.Sleep(TimeSpan.FromSeconds(1));
+                    }
+                }
+
+            } while (true);
+        }
+    }
+}
