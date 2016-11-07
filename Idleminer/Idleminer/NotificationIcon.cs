6ee5c39be1df676df4f7d0725b8247847a0d3178
Nilotaviano <nilotaviano@gmail.com>
2016-11-07 01:00:33 -0300

    Converted to winforms
    Converted to winforms, making it run as a system tray program.

---
+++ Idleminer/Idleminer/NotificationIcon.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+using Win32_API;
+
+namespace Idleminer
+{
+    public partial class NotificationIcon : Form
+    {
+        private readonly TimeSpan timeToTrigger = new TimeSpan(hours: 0, minutes: 5, seconds: 0);
+        private readonly TimeSpan intervalToCheck = new TimeSpan(hours: 0, minutes: 0, seconds: 5);
+
+        public NotificationIcon(string path)
+        {
+            timer = new System.Timers.Timer(intervalToCheck.TotalMilliseconds);
+
+            processInfo = new ProcessStartInfo(path)
+            {
+                CreateNoWindow = false
+            };
+
+            InitializeComponent();
+
+            Disposed += _Disposed; ;
+        }
+
+        private System.Timers.Timer timer;
+
+        private ProcessStartInfo processInfo;
+        private Process process;
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            Hide();
+
+            timer.Enabled = true;
+            timer.Elapsed += Timer_Elapsed;
+        }
+
+        private void _KillProcess()
+        {
+            process?.CloseMainWindow();
+            process?.WaitForExit();
+            process = null;
+        }
+
+        private void Timer_Elapsed(object sender, EventArgs e)
+        {
+            var idleTime = Win32.GetIdleTime();
+
+            if (idleTime < timeToTrigger.TotalMilliseconds)
+            {
+                _KillProcess();
+            }
+            else
+            {
+                if (process == null)
+                {
+                    try
+                    {
+                        process = Process.Start(processInfo);
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString(), "Error");
+                        Close();
+                    }
+                }
+                else
+                {
+                    Thread.Sleep(TimeSpan.FromSeconds(1));
+                }
+            }
+        }
+
+        private void _Disposed(object sender, EventArgs e)
+        {
+            _KillProcess();
+            Close();
+        }
+    }
+}
--- Idleminer/Idleminer/Program.cs
+++ Idleminer/Idleminer/Program.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using Win32_API;
+﻿using System.Linq;
+using System.Windows.Forms;
 
 namespace Idleminer
 {
@@ -15,48 +9,9 @@ namespace Idleminer
 
         static void Main(string[] args)
         {
-            string path = args.FirstOrDefault();
-            if (string.IsNullOrWhiteSpace(path))
-            {
-                Console.WriteLine(NO_PATH_SPECIFIED_MESSAGE);
-                path = Console.ReadLine();
-            }
-
-            var timeToTrigger = new TimeSpan(hours: 0, minutes: 5, seconds: 0);
-            var sleepTime = new TimeSpan(hours: 0, minutes: 1, seconds: 0);
-
-            var processInfo = new ProcessStartInfo(path)
-            {
-                CreateNoWindow = false
-            };
-
-            Process process = null;
-
-            do
-            {
-                var idleTime = Win32.GetIdleTime();
-
-                if (idleTime < timeToTrigger.TotalMilliseconds)
-                {
-                    process?.CloseMainWindow();
-                    process?.WaitForExit();
-                    process = null;
-
-                    Thread.Sleep((int)sleepTime.TotalMilliseconds);
-                }
-                else
-                {
-                    if (process == null)
-                    {
-                        process = Process.Start(processInfo);
-                    }
-                    else
-                    {
-                        Thread.Sleep(TimeSpan.FromSeconds(1));
-                    }
-                }
-
-            } while (true);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new NotificationIcon(args.FirstOrDefault()));
         }
     }
 }
